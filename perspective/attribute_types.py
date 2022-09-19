f72887b2e4ecbc5afafa6225579000e8b5537f57
Sea-Snell <sea_snell@icloud.com>
2022-09-18 17:49:55 -0700

    changed types

--- perspective/attribute_types.py
+++ perspective/attribute_types.py
@@ -1,11 +1,12 @@
-from typing import Dict, List, Literal, Optional, Union
+# from typing import Dict, List, Literal, Optional, Union
+from typing import Any
 
-COMMENT_TYPE = Optional[Literal['PLAIN_TEXT', 'HTML']]
-CONTEXT = Optional[List[Dict[Literal["text", "type"], str]]]
-REQUESTED_ATTRIBUTES = Optional[Dict[str, Dict[Literal["scoreType", "scoreThreshold"], Union[str, float]]]]
-LANGUAGES = Optional[List[str]]
-SUMMARY_SCORE = Union[Dict[Literal["value"], float], Dict[Literal["type"], str]]
-SPAN_SCORE_POSITION = Dict[Literal["begin", "end"], int]
-SPAN_SCORE_VALUE = Dict[Literal["score"], Union[Dict[Literal["value"], float], Dict[Literal["type"], str]]]
-SPAN_SCORES = List[Union[SPAN_SCORE_POSITION, SPAN_SCORE_VALUE]]
-ATTRIBUTE_SCORES = Dict[str, Union[SUMMARY_SCORE, SPAN_SCORES]]
+COMMENT_TYPE = Any # Optional[Literal['PLAIN_TEXT', 'HTML']]
+CONTEXT = Any # Optional[List[Dict[Literal["text", "type"], str]]]
+REQUESTED_ATTRIBUTES = Any # Optional[Dict[str, Dict[Literal["scoreType", "scoreThreshold"], Union[str, float]]]]
+LANGUAGES = Any # Optional[List[str]]
+SUMMARY_SCORE = Any # Union[Dict[Literal["value"], float], Dict[Literal["type"], str]]
+SPAN_SCORE_POSITION = Any # Dict[Literal["begin", "end"], int]
+SPAN_SCORE_VALUE = Any # Dict[Literal["score"], Union[Dict[Literal["value"], float], Dict[Literal["type"], str]]]
+SPAN_SCORES = Any # List[Union[SPAN_SCORE_POSITION, SPAN_SCORE_VALUE]]
+ATTRIBUTE_SCORES = Any # Dict[str, Union[SUMMARY_SCORE, SPAN_SCORES]]
